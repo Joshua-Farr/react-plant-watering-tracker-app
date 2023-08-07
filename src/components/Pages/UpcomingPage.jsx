a118396df2cb87b1664a8d07aa0237032b638188
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2023-08-07 17:57:21 -0500

    Adding styling for plant cards

--- src/components/Pages/UpcomingPage.jsx
+++ src/components/Pages/UpcomingPage.jsx
@@ -26,6 +26,48 @@ export default function UpcomingPage() {
           Tap on each task for instructions
         </h3>
       </div>
+      <div className="upcoming-reminder-card">
+        <div>
+          <h3 className="time-left-until-action">In 13 days</h3>
+          <h4 className="tasks-remaining">1 task</h4>
+        </div>
+        <div className="upcoming-plant-information">
+          <div className="icon-circle">
+            <img src="" alt="" />
+          </div>
+          <div className="reminder-info-text">
+            <h3>Orchid</h3>
+            <h4 className="reminder-info-location">Living Room</h4>
+          </div>
+          <div className="task-bubble">
+            <WaterDropRoundedIcon size="large" />
+          </div>
+        </div>
+        <h3 className="more-info-reminder">
+          Tap on each task for instructions
+        </h3>
+      </div>
+      <div className="upcoming-reminder-card">
+        <div>
+          <h3 className="time-left-until-action">In 21 days</h3>
+          <h4 className="tasks-remaining">3 task</h4>
+        </div>
+        <div className="upcoming-plant-information">
+          <div className="icon-circle">
+            <img src="" alt="" />
+          </div>
+          <div className="reminder-info-text">
+            <h3>Succulent</h3>
+            <h4 className="reminder-info-location">Kitchen</h4>
+          </div>
+          <div className="task-bubble">
+            <WaterDropRoundedIcon size="large" />
+          </div>
+        </div>
+        <h3 className="more-info-reminder">
+          Tap on each task for instructions
+        </h3>
+      </div>
     </>
   );
 }
