a0577ca83ddfac3f1c87d2418fd793e1e1b8e953
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2023-08-07 19:46:02 -0500

    Form Outline Completed

--- src/components/Pages/NewPlantInfoPage.jsx
+++ src/components/Pages/NewPlantInfoPage.jsx
@@ -1,22 +1,26 @@
+import "../../styling/NewPlantInfoPage.css";
+import { NavLink } from "react-router-dom";
 import React from "react";
 
 export default function NewPlantInfoPage() {
   return (
     <div className="my-plants-container">
       <h1>Lets add a new plant to your collection! 🌱</h1>
-      <h3></h3>
+      <img
+        src="https://images.pexels.com/photos/3153526/pexels-photo-3153526.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        className="plant-form-img"
+      />
       <div className="add-new-plant-form">
         <form action="">
-          {/* <input type="file" name="file" /> */}
           <label>
             Plant Name:
-            <input type="text" />
+            <input type="text" className="plant-name-input" />
           </label>
 
           <label>
             Location:
             <select>
-              <option>Select Option</option>
+              <option>Select</option>
               <option>Outside</option>
               <option>Office</option>
               <option>Kitchen</option>
@@ -31,21 +35,26 @@ export default function NewPlantInfoPage() {
 
           <label>
             When did you last water this plant?
-            <input type="date" />
+            <input type="date" className="plant-form-date" />
           </label>
 
           <label>
             How often does it need watering?
             <select>
-              <option>Select Option</option>
+              <option>Select</option>
               <option>Every week</option>
               <option>Every other week</option>
               <option>Once a month</option>
               <option>Every other month</option>
             </select>
           </label>
-          <button className="submit-button">Add Plant</button>
         </form>
+        <div className="plant-form-buttons">
+          <NavLink to="../plants" relative="path">
+            <button className="cancel-button">Cancel</button>
+          </NavLink>
+          <button className="submit-button">Submit</button>
+        </div>
       </div>
     </div>
   );
