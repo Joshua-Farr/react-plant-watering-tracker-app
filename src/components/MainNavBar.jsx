9e7bf6b6dae32e7e208956a28b4b4a064760b9fd
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2023-08-13 21:01:22 -0500

    Fixed NavLink Styling Issues

--- src/components/MainNavBar.jsx
+++ src/components/MainNavBar.jsx
@@ -3,7 +3,7 @@ import HomeIcon from "@mui/icons-material/Home";
 import SearchRoundedIcon from "@mui/icons-material/Search";
 import SettingsRoundedIcon from "@mui/icons-material/Settings";
 import YardRoundedIcon from "@mui/icons-material/YardRounded";
-import SpaRoundedIcon from "@mui/icons-material/SpaRounded";
+import LocalFloristRoundedIcon from "@mui/icons-material/LocalFloristRounded";
 import AddCircleOutlineRoundedIcon from "@mui/icons-material/AddCircleOutlineRounded";
 import { Link, NavLink } from "react-router-dom";
 import "../styling/MainNavBar.css";
@@ -11,27 +11,28 @@ import "../styling/MainNavBar.css";
 export default function MainNavBar() {
   return (
     <div className="menu-bar-wrapper">
-      <NavLink
-        to="home-nav"
-        className={({ isActive }) =>
-          isActive ? "nav-bar-link selected" : "nav-bar-link"
-        }
-      >
-        <div className="menu-item">
-          <HomeIcon />
-          <h4>Home</h4>
-        </div>
+      <NavLink to="home-nav" style={{ textDecoration: "none" }}>
+        {({ isActive }) => (
+          <div
+            className="menu-item"
+            style={{ color: isActive ? "#7da08e" : `#969696` }}
+          >
+            <HomeIcon />
+            <h4>Home</h4>
+          </div>
+        )}
       </NavLink>
-      <NavLink
-        to="plants"
-        className={({ isActive }) =>
-          isActive ? "nav-bar-link selected" : "nav-bar-link"
-        }
-      >
-        <div className="menu-item">
-          <SpaRoundedIcon />
-          <h4>My Plants</h4>
-        </div>
+
+      <NavLink to="plants" style={{ textDecoration: "none" }}>
+        {({ isActive }) => (
+          <div
+            className="menu-item"
+            style={{ color: isActive ? "#7da08e" : `#969696` }}
+          >
+            <LocalFloristRoundedIcon />
+            <h4>My Plants</h4>
+          </div>
+        )}
       </NavLink>
       {/* <NavLink
         to="search"
