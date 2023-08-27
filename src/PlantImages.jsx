7f6f48d0b014971c02b824f13622c088940628cd
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2023-08-27 10:23:52 -0500

    Fixed Nav-bar issues on PlantPage

--- src/PlantImages.jsx
+++ src/PlantImages.jsx
@@ -6,5 +6,6 @@ export const plantArray = [
   "https://images.pexels.com/photos/2778192/pexels-photo-2778192.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   "https://images.pexels.com/photos/3904880/pexels-photo-3904880.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   "https://images.pexels.com/photos/8988961/pexels-photo-8988961.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  "https://images.pexels.com/photos/3153526/pexels-photo-3153526.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/1084188/pexels-photo-1084188.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/2123482/pexels-photo-2123482.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 ];
